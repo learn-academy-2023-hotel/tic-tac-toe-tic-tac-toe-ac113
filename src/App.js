0898b1838bc91cd7cc29ae7428512562e90180c0
adriancarranza <adriancarranza0113@gmail.com>
2023-12-27 21:36:01 -0800

    the one where the second player marks

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Square from './components/Square'
 import './App.css'
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-
   return (
     <>
       <h1>Tic Tac Toe</h1>
